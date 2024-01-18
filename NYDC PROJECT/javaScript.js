46c9cee0ca0720c2215a5ad112a44b7d7e4e5818
AlhanCreation <126331253+AlhanCreation@users.noreply.github.com>
2024-01-18 19:14:52 +0530

    Add files via upload
    start

---
+++ NYDC PROJECT/javaScript.js
@@ -0,0 +1,32 @@
+
+const wrapper = document.querySelector(".wrapper");
+const login = document.querySelector(".login-link");
+const registerLink =document.querySelector(".register-link");
+const LoginBtn =document.querySelector(".btnLogin-popup");
+const iconClose =document.querySelector(".icon-close");
+// const RegisterBtn =document.querySelector(".icon-close");
+const LoginBtnAccepet =document.querySelector(".btn");
+
+
+registerLink.addEventListener('click',()=> {
+	wrapper.classList.add('active');
+});
+
+
+login.addEventListener('click',()=> {
+	wrapper.classList.remove('active');
+});
+
+LoginBtn.addEventListener('click',()=> {
+	wrapper.classList.add('active-btn');
+	
+});
+
+iconClose.addEventListener('click',()=> {
+	wrapper.classList.remove('active-btn');
+});
+
+LoginBtnAccepet.addEventListener('click',()=> {
+	wrapper.classList.remove('active-btn');
+	document.getElementById("changeText").innerHTML="Logout";
+});
